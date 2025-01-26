b6f764b12a9e338d06424b25ba717888d1ade191
unknown <jagotabrahm@gmail.com>
2025-01-26 18:37:38 +0530

    added roadmap page

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,12 +50,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
           {children}
         </body>
       </html>

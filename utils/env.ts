ecec2559c12cf90d5817e9e7853200f341128968
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-01-26 11:55:33 +0100

    change package manager and add env validation

---
+++ utils/env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string(),
+    CLERK_SECRET_KEY: z.string(),
+  },
+  clientPrefix: "NEXT_PUBLIC_",
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
+});

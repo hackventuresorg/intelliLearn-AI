8c304468c9e1c0534f1edd2c8f0cec228cc4d0ac
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-01-26 12:32:05 +0100

    feat(clerk): create webhook for user creation and deletion

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
     CLERK_SECRET_KEY: z.string(),
+    CLERK_SIGNING_SECRET: z.string(),
   },
   clientPrefix: "NEXT_PUBLIC_",
   client: {
@@ -15,5 +16,6 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_SIGNING_SECRET: process.env.CLERK_SIGNING_SECRET,
   },
 });
